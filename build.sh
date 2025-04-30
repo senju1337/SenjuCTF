40533be38899059e622c21d02552bba96ab7ee87
0xalivecow <0xalivecow@ta-lotz.de>
2025-04-30 20:18:18 +0200

    feat: add jekyll builder to build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@
 #GID=$(id -g)
 set -e
 
+cd ./jekyll_builder
+docker build -t jekyll .
+
+cd ..
+
 rm -rf _site
 echo "
+bash tools/init.sh \
 JEKYLL_ENV=production bundle exec jekyll build --no-watch
 rm -vrf /app/_site/docker* /app/_site/entrypoint.sh
 " \
 | docker run --rm -iv "$PWD:/app:z" jekyll /bin/bash
 
 docker build -t senju_ctf .
-
